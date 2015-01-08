cf2035b974a5ab4883963c7d3ea8d41e76c8a1ea
fabs <fyamagu@gwdg.de>
2015-01-08 22:05:34 +0100

    Added queryNodeIndex.

---
+++ python-bjoern/bjoern/bjoernsteps/lookup.groovy
@@ -0,0 +1,22 @@
+/**
+   This module contains index lookup functions employed to provide
+   start node sets for traversals. All of these lookups support wild
+   cards (you will need to escape spaces though).
+*/
+
+
+/**
+   Retrieve nodes from index using a Lucene query.
+
+   @param query The lucene query to run
+
+*/
+
+Object.metaClass.queryNodeIndex = { query
+	index = g.getRawGraph().index().forNodes(NODE_INDEX)
+	try{
+	    new Neo4j2VertexIterable(index.query(query), g)._()
+	}catch(ParseException){
+	  return []._()
+	}
+}
