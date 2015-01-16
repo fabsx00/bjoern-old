f27d8d24959b9fb954e2df614166246ffb158229
fabs <fyamagu@gwdg.de>
2015-01-16 14:56:34 +0100

    Cleanup: renamed astExporter to exporter, adapted build.

--- roseExporter/build.sh
+++ roseExporter/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-g++ -I/home/bjoern/rose/include -I/home/bjoern/rose/include/rose -I/home/bjoern/boost_47_153/include -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"astExporter.d" -MT"astExporter.d" -o "astExporter.o" "astExporter.cpp"
+g++ -I/home/bjoern/rose/include -I/home/bjoern/rose/include/rose -I/home/bjoern/boost_47_153/include -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"exporter.d" -MT"exporter.d" -o "exporter.o" "exporter.cpp"
 
 g++ -I/home/bjoern/rose/include -I/home/bjoern/rose/include/rose -I/home/bjoern/boost_47_153/include -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"CSVWriter.d" -MT"CSVWriter.d" -o "CSVWriter.o" "CSVWriter.cpp"
 
-g++ -L/home/bjoern/boost_47_153/lib -L/home/bjoern/rose/lib -o "astExporter"  ./astExporter.o ./CSVWriter.o  -lrose -lboost_thread -lboost_filesystem -lboost_wave -lboost_iostreams -lboost_system -lboost_regex
+g++ -L/home/bjoern/boost_47_153/lib -L/home/bjoern/rose/lib -o "exporter"  ./exporter.o ./CSVWriter.o  -lrose -lboost_thread -lboost_filesystem -lboost_wave -lboost_iostreams -lboost_system -lboost_regex
--- roseExporter/export.sh
+++ roseExporter/export.sh
@@ -3,5 +3,5 @@
 BATCH_IMPORTER_JAR=/home/bjoern/neo4j/batch-import-jar-with-dependencies.jar
 
 source ./set_rose_env.sh
-./astExporter "$@"
+./exporter "$@"
 java -jar $BATCH_IMPORTER_JAR /home/bjoern/neo4j/graph.db nodes.csv edge.csv
