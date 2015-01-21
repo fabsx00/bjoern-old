6a046ae1b6eae07517dd801f53bac84a25607fa7
fabs <fyamagu@gwdg.de>
2015-01-21 17:25:24 +0100

    Fixed path in astExporter.sh
    util -> utils.

--- exporters/bapExporter/astExporter.sh
+++ exporters/bapExporter/astExporter.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
+$BAPDIR=/home/user/bap-0.8/utils
+
 # Pass on command-line-arguments
-/home/user/bap-0.8/util/toil -bin $@ -o il.txt
+$BAPDIR/toil -bin $@ -o il.txt
 
 # Get CFG - as a dot graph...
-/home/user/bap-0.8/util/iltrans -il il.txt -to-cfg -pp-ast-cfg cfg.dot
+$BAPDIR/iltrans -il il.txt -to-cfg -pp-ast-cfg cfg.dot
 
 # Get SSA-DDG - as a dot graph... with basic block granularity
-/home/user/bap-0.8/util/iltrans -il il.txt -to-ssa -pp-ssa-ddg ssa_ddg.dot
-
+$BAPDIR/iltrans -il il.txt -to-ssa -pp-ssa-ddg ssa_ddg.dot
 
 ./to_csv cfg.dot ssa_ddg.dot
 
