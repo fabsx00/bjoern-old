6156b83dfc3cef885569a57abc2a66803a57eaf1
fabs <fyamagu@gwdg.de>
2015-01-28 15:13:21 +0100

    functionToInstructions.

--- python-bjoern/bjoern/bjoernsteps/function.groovy
+++ python-bjoern/bjoern/bjoernsteps/function.groovy
@@ -2,3 +2,7 @@
 Gremlin.defineStep('functionToBasicBlocks', [Vertex,Pipe], {
 	_().transform{ q = "type:BasicBlock AND functionId:" + it.id; queryNodeIndex(q); }.scatter()
 })
+
+Gremlin.defineStep('functionToInstructions', [Vertex,Pipe], {
+	_().functionToBasicBlocks().out().filter{it.type == "Instruction"}
+})
