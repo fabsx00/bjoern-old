9c90feb7489f15fab1a831e8347e947b4677ea4a
fabs <fyamagu@gwdg.de>
2015-03-31 18:23:15 +0200

    Adaption of roseExporter to incorporate PSS code

--- exporters/roseExporter/exporter.cpp
+++ exporters/roseExporter/exporter.cpp
@@ -14,6 +14,7 @@
 
 #include "bjoernNodes.hpp"
 #include "CSVWriter.hpp"
+#include "bjoernUseDefAnalyzer.hpp"
 
 using namespace rose;
 using namespace rose::BinaryAnalysis;
@@ -45,13 +46,18 @@ class MyProcessor : public SgSimpleProcessing
 {
 private:
 	CSVWriter writer;
+	BjoernUseDefAnalyzer useDefAnalyzer;
 public:
 	MyProcessor()
 	{
-		writer.init();
 	}
 
 	~MyProcessor(){}
+
+	void init(const SgAsmGenericFile* asmFile)
+	{
+		writer.init();
+	}
 
 	/**
 	   Called for each node of the "binary AST"
@@ -72,6 +78,8 @@ public:
 		BjoernFunctionNode *bjoernFunc = createBjoernFuncFromSgFunc(func);
 		writer.writeFunction(bjoernFunc);
 		delete bjoernFunc;
+
+		useDefAnalyzer.analyze(func);
 	}
 
 	BjoernFunctionNode *createBjoernFuncFromSgFunc(SgAsmFunction *func)
@@ -184,17 +192,47 @@ public:
 
 };
 
+
+SgBinaryComposite *getBinary(SgProject *project)
+{
+	auto fileList = project->get_fileList();
+	if (fileList.size() != 1) {
+		cerr << "No or multiple input files given." << endl;
+		return nullptr;
+	}
+
+	auto file = fileList[0];
+	SgBinaryComposite *binFile = isSgBinaryComposite(file);
+	if (fileList.size() != 1) {
+		cerr << "No or multiple input files given." << endl;
+		return nullptr;
+	}
+
+	return binFile;
+}
+
+/**
+
+   Entry point for exporter.
+   The exporter expects a path to a binary as its only argument.
+ */
+
 int main(int argc, char *argv[]) {
 
 	/* Create a binary AST for the entire binary */
 	SgProject* myProject = frontend(argc, argv);
 
-	/* Get a reference to the root node*/
+	/* Get a reference to the root node */
 	SgNode*  rootNode = dynamic_cast<SgNode*>(myProject);
+
+	auto binFile = getBinary(myProject);
+	if(binFile == nullptr)
+		return 1;
 
 	/* Traverse AST in post-order */
 
 	MyProcessor mp;
+	mp.init(binFile->get_binaryFile());
 	t_traverseOrder order = postorder;
 	mp.traverse(rootNode, order);
 }
